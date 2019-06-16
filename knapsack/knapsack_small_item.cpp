1e51f08467b37047db565cdedbcc01564259c711
qisnotnq <qisnotnq@gmail.com>
2019-06-16 12:34:34 +0900

    add small item version

---
+++ knapsack/knapsack_small_item.cpp
@@ -0,0 +1,52 @@
+#include <algorithm> // max
+#include <climits> // ULLONG_MAX
+#include <utility> // pair
+#include <vector> // vector
+
+using ull = unsigned long long;
+
+// First element is weight, second element is value.
+using Item = std::pair<ull, ull>;
+ 
+// helper function
+template <class ItemIterator>
+std::vector<Item> _knapsack_bitdp(ItemIterator first, ItemIterator last, ull capacity) {
+    int n = distance(first, last);
+    std::vector<Item> a;
+    for (int b = 0; b < (1 << n); ++b) {
+        ull sum_value = 0;
+        ull sum_weight = 0;
+        for (int i = 0; i < b; ++i) {
+            if (b & (1 << i)) {
+                sum_weight += (*(first + i)).first;
+                sum_value += (*(first + i)).second;
+            }
+        }
+        if (sum_weight <= capacity) {
+            a.push_back(Item(sum_weight, sum_value));
+        }
+    }
+    return a;
+}
+
+ull knapsack(std::vector<Item> &items, ull capacity) {
+    int n = items.size();
+    std::vector<Item> a1 = _knapsack_bitdp(items.begin(), items.begin() + n / 2, capacity);
+    std::vector<Item> a2 = _knapsack_bitdp(items.begin() + n / 2, items.end(), capacity);
+    sort(a2.begin(), a2.end());
+ 
+    std::vector<ull> acc_max_value(a2.size() + 1);
+    acc_max_value[0] = 0;
+    for (int i = 0; i < a2.size(); ++i) {
+        acc_max_value[i + 1] = std::max(acc_max_value[i], a2[i].second);
+    }
+ 
+    ull result = 0;
+    for (Item item: a1) {
+        ull weight1 = item.first;
+        ull value1 = item.second;
+        ull value2 = acc_max_value[distance(a2.begin(), upper_bound(a2.begin(), a2.end(), Item(capacity - weight1, ULLONG_MAX)))];
+        result = std::max(result, value1 + value2);
+    }
+    return result;
+}
