6cac4dd01a84a4c6c7e20dd0b45fbd7ceafa35b4
qisnotnq <qisnotnq@gmail.com>
2019-06-16 12:06:17 +0900

    add score resolution

--- knapsack/knapsack.cpp
+++ knapsack/knapsack.cpp
@@ -1,20 +1,27 @@
+#include <utility>
+#include <vector>
+
 using ull = unsigned long long;
 
 // First element is weight, second element is value.
-using Item = pair<int, ull>; 
+using Item = std::pair<int, ull>; 
 
 // Time Complexity: O(items.size() * capacity)
 // Space Complexity: O(capacity)
-ull knapsack(vector<Item> &items, int capacity) {
-    vector<ull> dp(capacity + 1, 0);
+ull knapsack(std::vector<Item> &items, int capacity) {
+    std::vector<ull> dp(capacity + 1, 0);
     for (int i = 1; i <= capacity; ++i) {
         for (Item item: items) {
             int weight = item.first;
             ull value = item.second;
             if (weight <= i) {
-                dp[i] = max(dp[i], dp[i - weight] + value);
+                dp[i] = std::max(dp[i], dp[i - weight] + value);
             }
         }
     }
     return dp[capacity];
+}
+
+int main() {
+    return 0;
 }
