baa5f199d7fccba94917ebcf2c21ea2c1fca6496
qisnotnq <qisnotnq@gmail.com>
2019-06-16 12:49:32 +0900

    rename small_items

--- knapsack/knapsack_small_items.cpp
+++ knapsack/knapsack_small_items.cpp
@@ -33,7 +33,7 @@ std::vector<Item> _knapsack_bitdp(ItemIterator first, ItemIterator last, ull cap
 // auxiliary space is O(m * log(m)),
 // where n = items.size(),
 //       m = pow(2, n / 2).
-ull knapsack(std::vector<Item> &items, ull capacity) {
+ull knapsack_small_items(std::vector<Item> &items, ull capacity) {
     int n = items.size();
     std::vector<Item> a1 = _knapsack_bitdp(items.begin(), items.begin() + n / 2, capacity);
     std::vector<Item> a2 = _knapsack_bitdp(items.begin() + n / 2, items.end(), capacity);
