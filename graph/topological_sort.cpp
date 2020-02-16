e63e37fc60fa10b2248509fe9d8255a85f47babe
qisnotnq <qisnotnq@gmail.com>
2020-02-17 00:13:52 +0900

    new top sort

---
+++ graph/topological_sort.cpp
@@ -0,0 +1,44 @@
+#include <vector> // std::vector
+#include <queue> // std::queue
+
+// time complexity: O(V + E)
+template <class Graph>
+std::vector<size_t> topological_sort(const Graph &g) {
+
+    using Edge = typename Graph::edge_type;
+
+    std::vector<size_t> topological_orders(g.n_vertices());
+    std::vector<size_t> indegrees(g.n_vertices());
+
+    for (size_t v = 0; v < g.n_vertices(); ++v) {
+        for (const Edge &e: g.edges_from(v)) {
+            ++indegrees[e.to()];
+        }
+    }
+
+    std::queue<size_t> q;
+
+    for (size_t v = 0; v < g.n_vertices(); ++v) {
+        if (!indegrees[v]) {
+            q.push(v);
+        }
+    }
+
+    for (size_t i = 0; i < g.n_vertices(); ++i) {
+        if (q.empty()) { // has cycle
+            return { };
+        }
+        size_t u = q.front();
+        q.pop();
+        topological_orders[i] = u;
+        for (const Edge &e: g.edges_from(u)) {
+            size_t v = e.to();
+            --indegrees[v];
+            if (!indegrees[v]) {
+                q.push(v);
+            }
+        }
+    }
+
+    return topological_orders;
+}
