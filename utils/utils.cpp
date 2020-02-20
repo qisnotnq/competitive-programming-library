5ae1fcb1a99194f95e9808d99b2701ffff595d9f
qisnotnq <qisnotnq@gmail.com>
2020-02-21 04:17:29 +0900

    new utils.cppJ

---
+++ utils/utils.cpp
@@ -0,0 +1,17 @@
+#ifndef UTILS_CPP
+#define UTILS_CPP
+
+#include <algorithm> // std::sort
+#include <vector> // std::vector
+
+template <class T>
+std::vector<size_t> argsort(std::vector<T> &a) {
+    std::vector<size_t> result(a.size());
+    for (size_t i = 0; i < result.size(); ++i) {
+        result[i] = i;
+    }
+    std::sort(result.begin(), result.end(), [&](size_t i, size_t j) { return a[i] < a[j]; });
+    return result;
+}
+
+#endif // UTILS_CPP
