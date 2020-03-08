b111938b6389c15d8e6d8afb4c7857ba859cc0ea
qisnotnq <qisnotnq@gmail.com>
2020-03-09 02:34:54 +0900

    add ceil_div

--- utils/utils.cpp
+++ utils/utils.cpp
@@ -14,4 +14,25 @@ std::vector<size_t> argsort(const std::vector<T> &a) {
     return result;
 }
 
+long long ceil_div(long long a, long long b) {
+    if (b > 0) {
+        if (a >= 0) {
+            return (a + b - 1) / b;
+        } else {
+            // ceil(a / b) == ceil(-((-a) / b)) == -floor((-a) / b)
+            return -((-a) / b);
+        }
+    } else if (b < 0) {
+        if (a >= 0) {
+            // ceil(a / b) == ceil(-(a / (-b))) == -floor(a / (-b))
+            return -(a / (-b));
+        } else {
+            // ceil(a / b) == ceil((-a) / (-b))
+            return ((-a) + (-b) - 1) / (-b);
+        }
+    } else {
+        throw runtime_error("division by zero");
+    }
+}
+
 #endif // UTILS_CPP
